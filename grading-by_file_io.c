c4ec38364a537bef6586a65d9de7e037113169af
WhyJS <dbs106@gmail.com>
2020-02-12 00:17:03 +0900

    파일포인터 관련하여 전체적인 코드 구조 수정이 필요함

--- grading-by_file_io.c
+++ grading-by_file_io.c
@@ -1,3 +1,5 @@
+// fscanf와 fprintf가 파일포인터를 공유하기 때문에 파일포인터가 옮겨질때 같이 사용해줘야함
+//따라서 전체적인 구조를 수정해야한다.
 #include<stdio.h>
 #include<string.h>
 //Input: 학번 과제1(10%) 중간고사(30%) 과제2(20%) 기말고사(40%)
@@ -13,12 +15,12 @@ char* grading(int avr);
 void cal_make_output(FILE* input, FILE* output);
 
 int main(){
-    FILE* If = fopen("grading_file_io_c\\input.txt","w");
+    FILE* If = fopen("grading_file_io_c/input.txt","w");
     if(!If){
         printf("Error on opening input file\n");
         return 100;
     }
-    FILE* Of = fopen("grading_file_io_c\\output.txt","w");
+    FILE* Of = fopen("grading_file_io_c/output.txt","w");
     if(!Of){
         printf("Error on opening output file\n");
         return 101;
@@ -35,7 +37,6 @@ int main(){
 void make_input(FILE* input){
     int ID, hw1, mid, hw2, fin;
     float avr;
-    char grade[10];
     for(int i=0; i<num; i++){
         printf("ID: ");
         scanf("%d", &ID);
@@ -48,9 +49,9 @@ void make_input(FILE* input){
         printf("Finalterm Exam: ");
         scanf("%d", &fin);
         fprintf(input, "%d %d %d %d %d", ID, hw1, mid, hw2, fin);
-        if(i!=num-1)
-            fprintf(input, "\n");
+        fprintf(input, "\n");
     }
+    fflush(input);
 }
 
 char* grading(int avr){
@@ -98,10 +99,11 @@ void cal_make_output(FILE* input, FILE* output){
         int id, hw1, mid, hw2, fin;
         float avr;
         char* grade;
+        //fscanf 사용시 file pointer 자동으로 증가!!!
         fscanf(input, "%d %d %d %d %d", &id, &hw1, &mid, &hw2, &fin);
+        printf("%d %d %d %d %d\n", id, hw1, mid, hw2, fin);
         avr = 0.1 * (float)hw1 + 0.3 * (float)mid + 0.2 * (float)hw2 + 0.4 * (float)fin;
         grade = grading(avr);
         fprintf(output, "%d %f %s\n", id, avr, grade);
     }    
 }
-
